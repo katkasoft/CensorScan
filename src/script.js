db672baa2bbc554a8564b5db6c00b5c7ed24f600
katkasoft <katkaleks@gmail.com>
2026-06-10 12:29:21 +0300

    Добавлен статус цензуры

--- src/script.js
+++ src/script.js
@@ -37,7 +37,12 @@ async function checkSite(url, timeout = 5000) {
     }
 }
 
+let working = []
+
 document.getElementById('check-button').addEventListener('click', async () => {
+    let censorStatus = document.getElementById('censor-status');
+    censorStatus.classList.remove('free', 'partial', 'whitelist', 'blackout');
+    censorStatus.innerText = "Проверяется..."
     const sites = document.querySelectorAll('.site');
     for (const site of sites) {
         const siteId = site.id;
@@ -46,9 +51,23 @@ document.getElementById('check-button').addEventListener('click', async () => {
         const result = await checkSite('https://' + siteId);
         if (result === 'working') {
             statusDiv.className = 'site-status working';
+            working.push(siteId)
 
         } else {
             statusDiv.className = 'site-status not-working';
         }
+    }
+    if (working.length == 6) {
+        censorStatus.innerText = "Свободный интернет";
+        censorStatus.classList.add('free');
+    } else if (working.length == 0) {
+        censorStatus.innerText = "Блекаут";
+        censorStatus.classList.add('blackout');
+    } else if ((!working.includes('telegram.org') || !working.includes('youtube.com')) && (working.includes('google.com') || working.includes('example.com'))) {
+        censorStatus.innerText = "Частично заблокированный интернет";
+        censorStatus.classList.add('partial');
+    } else if (!working.includes('telegram.org') && !working.includes('youtube.com') && !working.includes('google.com') && !working.includes('example.com') && working.includes('vk.com') && working.includes('yandex.ru')) {
+        censorStatus.innerText = "Белые списки";
+        censorStatus.classList.add('whitelist');
     }
 })
